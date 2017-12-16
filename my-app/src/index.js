496a35700b7399a761968097f00b33ad2a0e5eae
Jarek Pieprzak <jarekpiep@gmail.com>
2017-12-16 21:16:38 +0100

    changes that almost burned my computer -> something wrong

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -7,18 +7,48 @@ class Square extends React.Component {
     constructor(props) {
      super(props);
      this.state = {
-         value: null,
+         squares: Array(9).fill(null),
      };
     }
 
+    renderSquare(i) {
+        return <Square value={i} />;
+    }
+
     render() {
+        const status = 'Next player: X';
+
         return (
-            <button className="square" onClick={() => this.setState({value: 'X'})}>
-                {this.state.value}
-            </button>
-    );
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        );
     }
 }
+
+//     render() {
+//         return (
+//             <button className="square" onClick={() => this.setState({value: 'X'})}>
+//                 {this.state.value}
+//             </button>
+//     );
+//     }
+// }
 
 class Board extends React.Component {
     renderSquare(i) {
